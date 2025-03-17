4b1bc1d1d00e0e378279eefb6e81b0b15daac303
dfn96304 <s30496@pjwstk.edu.pl>
2025-03-17 18:42:59 +0100

    RefrigeratedContainer.cs

--- apbd-tut3/RefrigeratedContainer.cs
+++ apbd-tut3/RefrigeratedContainer.cs
@@ -19,16 +19,28 @@ public class RefrigeratedContainer : Container
     };
     
     public string? ProductType { get; set; }
+    public double Temperature { get; set; }
     
     public RefrigeratedContainer(int Height, int TareWeight, int Depth) : base(Height, TareWeight, Depth)
     {
         ProductType = null;
     }
     
-    public virtual void LoadContainer(int mass)
+    public void LoadContainer(string ProductType, int mass)
     {
         if (mass <= 0)
             throw new ArgumentOutOfRangeException("Mass cannot be negative or zero");
+
+        if (!RefrigeratedProducts.ContainsKey(ProductType))
+            throw new ArgumentException("Invalid product type");
+        
+        if (CargoMass == 0)
+            this.ProductType = ProductType;
+        else if(this.ProductType != ProductType)
+            throw new ArgumentException("This container is already loaded with a different product type");
+
+        if (this.Temperature < RefrigeratedProducts[ProductType])
+            throw new ArgumentException("The temperature of this container is lower than the required temperature for this product type");
         
         if(CargoMass+mass > MaxPayload)
             throw new OverflowException();
