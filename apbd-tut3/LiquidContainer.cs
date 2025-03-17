2fc3eca5e68942a0e20eb3d5b3bbfdc10a1ecb83
dfn96304 <s30496@pjwstk.edu.pl>
2025-03-17 18:12:02 +0100

    LiquidContainer.cs

--- apbd-tut3/LiquidContainer.cs
+++ apbd-tut3/LiquidContainer.cs
@@ -1,6 +1,15 @@
 ﻿namespace apbd_tut3;
 
-public class LiquidContainer
+public class LiquidContainer : Container
 {
+    public new const string ContainerType = "L";
+    
+    public bool HazardousCargo { get; set; }
+    
+    public LiquidContainer(int Height, int TareWeight, int Depth) : base(Height, TareWeight, Depth)
+    {
+        
+    }
+    
     
 }
